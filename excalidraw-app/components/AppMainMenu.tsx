a7888daa81a602c42e4d37735baf480f7d434006
Владислав Баталов <vbatalov1996@gmail.com>
2024-12-26 20:43:16 +0700

    MainMenu edit

--- excalidraw-app/components/AppMainMenu.tsx
+++ excalidraw-app/components/AppMainMenu.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import {
-  loginIcon,
-  ExcalLogo,
-  eyeIcon,
-} from "../../packages/excalidraw/components/icons";
-import type { Theme } from "../../packages/excalidraw/element/types";
-import { MainMenu } from "../../packages/excalidraw/index";
-import { isExcalidrawPlusSignedUser } from "../app_constants";
-import { LanguageList } from "../app-language/LanguageList";
-import { saveDebugState } from "./DebugCanvas";
+import type {Theme} from "../../packages/excalidraw/element/types";
+import {MainMenu} from "../../packages/excalidraw/index";
+import {LanguageList} from "../app-language/LanguageList";
 
 export const AppMainMenu: React.FC<{
   onCollabDialogOpen: () => any;
@@ -20,67 +13,66 @@ export const AppMainMenu: React.FC<{
 }> = React.memo((props) => {
   return (
     <MainMenu>
-      <MainMenu.DefaultItems.LoadScene />
-      <MainMenu.DefaultItems.SaveToActiveFile />
-      <MainMenu.DefaultItems.Export />
-      <MainMenu.DefaultItems.SaveAsImage />
+      <MainMenu.DefaultItems.LoadScene/>
+      <MainMenu.DefaultItems.SaveToActiveFile/>
+      <MainMenu.DefaultItems.Export/>
       {props.isCollabEnabled && (
         <MainMenu.DefaultItems.LiveCollaborationTrigger
           isCollaborating={props.isCollaborating}
           onSelect={() => props.onCollabDialogOpen()}
         />
       )}
-      <MainMenu.DefaultItems.CommandPalette className="highlighted" />
-      <MainMenu.DefaultItems.SearchMenu />
-      <MainMenu.DefaultItems.Help />
-      <MainMenu.DefaultItems.ClearCanvas />
-      <MainMenu.Separator />
-      <MainMenu.ItemLink
-        icon={ExcalLogo}
-        href={`${
-          import.meta.env.VITE_APP_PLUS_LP
-        }/plus?utm_source=excalidraw&utm_medium=app&utm_content=hamburger`}
-        className=""
-      >
-        Excalidraw+
-      </MainMenu.ItemLink>
-      <MainMenu.DefaultItems.Socials />
-      <MainMenu.ItemLink
-        icon={loginIcon}
-        href={`${import.meta.env.VITE_APP_PLUS_APP}${
-          isExcalidrawPlusSignedUser ? "" : "/sign-up"
-        }?utm_source=signin&utm_medium=app&utm_content=hamburger`}
-        className="highlighted"
-      >
-        {isExcalidrawPlusSignedUser ? "Sign in" : "Sign up"}
-      </MainMenu.ItemLink>
-      {import.meta.env.DEV && (
-        <MainMenu.Item
-          icon={eyeIcon}
-          onClick={() => {
-            if (window.visualDebug) {
-              delete window.visualDebug;
-              saveDebugState({ enabled: false });
-            } else {
-              window.visualDebug = { data: [] };
-              saveDebugState({ enabled: true });
-            }
-            props?.refresh();
-          }}
-        >
-          Visual Debug
-        </MainMenu.Item>
-      )}
-      <MainMenu.Separator />
+      <MainMenu.DefaultItems.CommandPalette className="highlighted"/>
+      <MainMenu.DefaultItems.SearchMenu/>
+      {/*<MainMenu.DefaultItems.Help />*/}
+      {/*<MainMenu.DefaultItems.ClearCanvas />*/}
+      <MainMenu.Separator/>
+      {/*<MainMenu.ItemLink*/}
+      {/*  icon={ExcalLogo}*/}
+      {/*  href={`${*/}
+      {/*    import.meta.env.VITE_APP_PLUS_LP*/}
+      {/*  }/plus?utm_source=excalidraw&utm_medium=app&utm_content=hamburger`}*/}
+      {/*  className=""*/}
+      {/*>*/}
+      {/*  Excalidraw+*/}
+      {/*</MainMenu.ItemLink>*/}
+      {/*<MainMenu.DefaultItems.Socials />*/}
+      {/*<MainMenu.ItemLink*/}
+      {/*  icon={loginIcon}*/}
+      {/*  href={`${import.meta.env.VITE_APP_PLUS_APP}${*/}
+      {/*    isExcalidrawPlusSignedUser ? "" : "/sign-up"*/}
+      {/*  }?utm_source=signin&utm_medium=app&utm_content=hamburger`}*/}
+      {/*  className="highlighted"*/}
+      {/*>*/}
+      {/*  {isExcalidrawPlusSignedUser ? "Sign in" : "Sign up"}*/}
+      {/*</MainMenu.ItemLink>*/}
+      {/*{import.meta.env.DEV && (*/}
+      {/*  <MainMenu.Item*/}
+      {/*    icon={eyeIcon}*/}
+      {/*    onClick={() => {*/}
+      {/*      if (window.visualDebug) {*/}
+      {/*        delete window.visualDebug;*/}
+      {/*        saveDebugState({ enabled: false });*/}
+      {/*      } else {*/}
+      {/*        window.visualDebug = { data: [] };*/}
+      {/*        saveDebugState({ enabled: true });*/}
+      {/*      }*/}
+      {/*      props?.refresh();*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    Visual Debug*/}
+      {/*  </MainMenu.Item>*/}
+      {/*)}*/}
+      {/*<MainMenu.Separator />*/}
       <MainMenu.DefaultItems.ToggleTheme
         allowSystemTheme
         theme={props.theme}
         onSelect={props.setTheme}
       />
       <MainMenu.ItemCustom>
-        <LanguageList style={{ width: "100%" }} />
+        <LanguageList style={{width: "100%"}}/>
       </MainMenu.ItemCustom>
-      <MainMenu.DefaultItems.ChangeCanvasBackground />
+      <MainMenu.DefaultItems.ChangeCanvasBackground/>
     </MainMenu>
   );
 });
